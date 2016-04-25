41eb609ebcc3a78fa3abe367e59aea2d9f906e23
Kavi Gupta <kavi@globalmbox.com>
2016-04-24 18:05:08 -0700

    Add timing code

--- erode.py
+++ erode.py
@@ -2,6 +2,17 @@ import numpy as np
 from config import *
 from random import randint
 from mathtools import gradAt
+from time import time
+
+time_matrix = {
+        'a' : 0,
+        'r' : 0,
+        'erode' : 0,
+        'in ocean' : 0,
+        's' : 0,
+        'calc_end' : 0,
+        'list' : 0
+    }
 
 def erode(mapp, rain, steps):
     size = mapp.width
@@ -20,20 +31,38 @@ def erode(mapp, rain, steps):
         S2 = []
         trails2 = []
         for i in range(len(R)):
+            t = time()
             a = -k_steep * gradAt(E, round(R[i][0]), round(R[i][1]))
+            time_matrix['a'] += time() - t
             v = a
+            t = time()
             r = (v + V[i]) / 2 + R[i]
-            if round(r[0]) < 0 or round(r[0]) >= size or round(r[1]) < 0 or round(r[1]) >= size or mapp.in_ocean(r):
+            time_matrix['r'] += time() - t
+            t = time()
+            end = round(r[0]) < 0 or round(r[0]) >= size or round(r[1]) < 0 or round(r[1]) >= size
+            time_matrix['calc_end'] += time() - t
+            if not end:
+                t = time()
+                end = mapp.in_ocean(r)
+                time_matrix['in ocean'] += time() - t
+            if end:
                 finished.append(trails[i])
                 continue
+            t = time()
+            ds = k_tough * (k_cap * np.linalg.norm(V[i]) - S[i])
+            s = S[i] + ds
+            time_matrix['s'] += time() - t
+            t = time()
+            mapp[R[i]] -= ds
+            time_matrix['erode'] += time() - t
+            t = time()
             trail = trails[i]
             trail.append(r)
-            s = S[i] + k_tough * (k_cap * np.linalg.norm(V[i]) - S[i])
-            mapp[R[i]] -= s - S[i]
             R2.append(r)
             V2.append(v)
             S2.append(s)
             trails2.append(trail)
+            time_matrix['list'] += time() - t
         V = V2
         R = R2
         S = S2
