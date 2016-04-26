86a4455553aa12849f5af657a0419954487c7e5e
Kavi Gupta <kavi@globalmbox.com>
2016-04-25 21:55:54 -0700

    removed v values

--- erode.py
+++ erode.py
@@ -9,18 +9,15 @@ def erode(mapp, rain, steps):
                     randint(0, size-1), randint(0, size-1)
                 ])
              for _ in range(rain)]
-    V = [np.zeros(2) for _ in range(rain)]
     S = [0 for _ in range(rain)]
     trails = [[] for _ in range(rain)]
     finished = []
     for count in range(steps):
         R2 = []
-        V2 = []
         S2 = []
         trails2 = []
         for i in range(len(R)):
-            a = -k_steep * gradAt(mapp.E, round(R[i][0]), round(R[i][1]))
-            v = a
+            v = -k_steep * gradAt(mapp.E, round(R[i][0]), round(R[i][1]))
             r = v + R[i]
             end = round(r[0]) < 0 or round(r[0]) >= size or round(r[1]) < 0 or round(r[1]) >= size
             if not end:
@@ -28,16 +25,14 @@ def erode(mapp, rain, steps):
             if end:
                 finished.append(trails[i])
                 continue
-            ds = k_tough * (k_cap * np.linalg.norm(V[i]) - S[i])
+            ds = k_tough * (k_cap * np.linalg.norm(v) - S[i])
             s = S[i] + ds
             mapp[R[i]] -= ds
             trail = trails[i]
             trail.append(r)
             R2.append(r)
-            V2.append(v)
             S2.append(s)
             trails2.append(trail)
-        V = V2
         R = R2
         S = S2
         trails = trails2
